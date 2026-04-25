a91b804733c9a74ab2c88abf9fffddc0ade4e0c7
p00n4k <105700844+p00n4k@users.noreply.github.com>
2026-04-26 02:56:02 +0700

    chnage image

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -36,7 +36,7 @@ export default function AboutPage() {
                         <div className="space-y-8">
                             <div className="relative w-full aspect-[4/3]">
                                 <Image
-                                    src="/static/getintouch_1.png"
+                                    src="/static/DSC09202.jpg"
                                     alt="Interior 1"
                                     fill
                                     className="object-cover rounded-lg"
@@ -45,7 +45,7 @@ export default function AboutPage() {
 
                             <div className="relative w-full aspect-[4/3]">
                                 <Image
-                                    src="/static/getintouch_2.png"
+                                    src="/static/DSC09178-HDR.jpg"
                                     alt="Interior 2"
                                     fill
                                     className="object-cover rounded-lg"
@@ -81,7 +81,7 @@ export default function AboutPage() {
 
                             <div className="relative w-full aspect-[3/4]">
                                 <Image
-                                    src="/static/getintouch_3.png"
+                                    src="/static/DSC09181.jpg"
                                     alt="Interior 3"
                                     fill
                                     className="object-cover rounded-lg"
